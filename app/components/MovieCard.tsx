418197030693c8c12c8885d41c6b00a69a86e058
siyasjr <siyasjr1@gmail.com>
2025-07-08 02:12:02 +0530

    tweak MovieCard

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -1,4 +1,7 @@
+import { title } from 'process';
 import React from 'react'
+
+
 
 const MovieCard = () => {
   return (
@@ -8,14 +11,14 @@ const MovieCard = () => {
     <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
     <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" // movie_poster_path
       alt="Shoes" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <h2 className="card-title">Movie Title</h2>
+    <p>Release Date</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      
     </div>
   </div>
 </div>
