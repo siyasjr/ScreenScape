2c227515f851cce7866b50c341ea968764bd5c07
siyasjr <siyasjr1@gmail.com>
2025-07-08 02:11:02 +0530

    Rearranged the Navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,39 +1,43 @@
-'use client';
-import Favorites from '../favorites/page';
+import React from 'react'
+import Link from 'next/link'
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
-import clsx from 'clsx';
-
-const Navbar: React.FC = () => {
-  const pathname = usePathname();
-
-  const links = [
-    { name: 'Home', href: '/' },
-    { name: 'MovieScape', href: '/moviescope' },
-    { name: 'Favorites', href: '/favorites' },
-  ];
-
+const NavBar  = () => {
   return (
-    <nav className="bg-#302f2f text-white py-4 flex items-center">
-      <h1 className="text-xl font-bold">🎬 MovieScape</h1>
-      <div className="flex gap-4 ml-auto">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={clsx(
-              'hover:text-blue-400 transition-colors',
-              pathname === link.href && 'text-blue-400 font-semibold'
-            )}
-          >
-            {link.name}
-          </Link>
-        ))}
+   <div className="navbar bg-base-200 shadow-lg">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
       </div>
-    </nav>
-  );
-};
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+ <Link className="btn btn-ghost text-xl" href={'/'}>MovieScape</Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><Link href={'/favorites'}> Favorites</Link></li> 
+      
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    
+    <a className="btn">Button</a>
+  </div>
+</div>
+  )
+}
 
-export default Navbar;
+export default NavBar
