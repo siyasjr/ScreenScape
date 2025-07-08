5fdd6dadd77ee2390c491068c15b2c256a2293c6
siyasjr <siyasjr1@gmail.com>
2025-07-08 14:13:21 +0530

    NavBar clutter cleared

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,41 +3,16 @@ import Link from 'next/link'
 
 const NavBar  = () => {
   return (
-   <div className="navbar bg-base-200 shadow-lg">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
- <Link className="btn btn-ghost text-xl" href={'/'}>MovieScape</Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><Link href={'/favorites'}> Favorites</Link></li> 
-      
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
+   <div className="navbar bg-base-200 shadow-lg flex justify-between px-10">
     
-    <a className="btn">Button</a>
-  </div>
+
+<Link className="btn btn-ghost text-xl" href={'/'}>MovieScape</Link>
+<Link className='text-lg' href={'/favorites'}> Favorites</Link>
+
+  
 </div>
   )
 }
 
 export default NavBar
+
