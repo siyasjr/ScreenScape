72ea9be401463688abeb3efb848f8f85e84bd49f
siyasjr <siyasjr1@gmail.com>
2025-07-14 23:37:03 +0530

    added message template for 'no favorites yet '

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Favorites = () => { 
   return (
-    <div> Favorites</div>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-base-300 rounded-2xl shadow-lg text-center">
+  <h1 className="text-2xl font-bold text-white mb-4">Favorites</h1>
+  <p className="text-gray-500 mb-2">No favorite movies here yet</p>
+  <p className="text-gray-500">Click on the 🤍 button to add to your favorites</p>
+</div>
+
   )
 }
 
