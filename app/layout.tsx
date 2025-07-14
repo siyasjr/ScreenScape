2450525ee12d82baa368d7b9f83ae6c99ea4f6ee
siyasjr <siyasjr1@gmail.com>
2025-07-14 23:34:28 +0530

    removed unused  fonts from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+
 import './globals.css'
 import NavBar from './components/NavBar'
 import { Providers } from './providers'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'MovieScape',
@@ -25,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="dim">
+    <html lang="en" data-theme="dim/">
       <body className="bg-base-100 text-base-content">
         <Providers>
           {/* ✅ Constrain content width and center */}
